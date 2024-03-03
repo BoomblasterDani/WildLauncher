d1bd73030924818848416764521da65a88ff0ec3
BoomblasterDani <62881289+BoomblasterDani@users.noreply.github.com>
2024-03-03 20:38:44 +0100

    tested Git

--- src/main/java/com/example/wildlauncher/HelloApplication.java
+++ src/main/java/com/example/wildlauncher/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.wildlauncher;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        VBox vBox = new VBox();
+        Button button = new Button("test");
+        vBox.getChildren().addAll(button);
+        Scene scene = new Scene(vBox, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
