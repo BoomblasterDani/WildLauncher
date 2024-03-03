3c5a088b280e9fc6033d85e41204083643cf801f
BoomblasterDani <62881289+BoomblasterDani@users.noreply.github.com>
2024-03-03 20:30:59 +0100

    Delete WildLauncher directory

--- WildLauncher/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.wildlauncher {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.wildlauncher to javafx.fxml;
-    exports com.example.wildlauncher;
-}
