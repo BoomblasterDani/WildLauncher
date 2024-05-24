100ac2af2922fa6dd34ceefc21f1d87a92f987b7
E10612 <danielkemf@gmail.com>
2024-05-24 20:02:36 +0200

    Task added to applet

--- src/main/java/com/example/wildlauncher/Applet.java
+++ src/main/java/com/example/wildlauncher/Applet.java
@@ -1,11 +1,10 @@
 package com.example.wildlauncher;
 
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 
 import javafx.scene.paint.Color;
@@ -13,7 +12,6 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
 import java.io.*;
-import java.net.URL;
 
 public class Applet extends Pane {
 
@@ -29,17 +27,17 @@ public class Applet extends Pane {
     public void buildApplet() {
         //Container for applet (rounded corners)
 
-        Rectangle rect = new Rectangle();
+        Rectangle background = new Rectangle();
 
-        rect.setHeight(100);
-        rect.setWidth(200);
-        rect.setArcHeight(15);
-        rect.setArcWidth(15);
-        rect.setFill(Color.LIGHTGREY);
+        background.setHeight(100);
+        background.setWidth(200);
+        background.setArcHeight(15);
+        background.setArcWidth(15);
+        background.setFill(Color.LIGHTGREY);
 
-        //rect.setClip(rect);
+        //background.setClip(background);
         /*if (application.running) {
-            rect.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+            background.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
         } else {
             this.setBackground(new Background(new BackgroundFill(Color.GREY, CornerRadii.EMPTY, Insets.EMPTY)));
         }*/
@@ -71,7 +69,14 @@ public class Applet extends Pane {
         appletUi.add(stopButton, 1, 1);
         appletUi.add(filesButton, 2, 1);
 
-        this.getChildren().addAll(rect, appletUi);
+        this.getChildren().addAll(background, appletUi);
+
+
+
+
+
+
+        // BUTTON EVENTLISTENERS
         filesButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
@@ -91,6 +96,61 @@ public class Applet extends Pane {
             }
         });
 
+
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Task<Integer> scriptTask = new Task<Integer>() {
+                    @Override
+                    protected Integer call() throws Exception {
+                        int id = 1;
+                        try {
+                            ProcessBuilder processBuilder = new ProcessBuilder();
+                            processBuilder.command("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe");
+                            Process process = processBuilder.start();
+                            id = process.waitFor();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                        return id;
+                    }
+                };
+
+                new Thread(scriptTask).start();
+                scriptTask.setOnSucceeded(e -> {
+                    background.setFill(Color.LIGHTBLUE);
+                });
+            }
+        });
+
+
+
+
+
+
+
+
+    }
+
+    // SERVICES
+    private void runScriptTask() {
+        Task<Integer> scriptTask = new Task<Integer>() {
+            @Override
+            protected Integer call() throws Exception {
+                int id = 1;
+                try {
+                    ProcessBuilder processBuilder = new ProcessBuilder();
+                    processBuilder.command("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe");
+                    Process process = processBuilder.start();
+                    id = process.waitFor();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                return id;
+            }
+        };
     }
 
     //public static List<Applet>
